59d42a4c96d45585549d4ff8330d56b5347e5056
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-01-04 10:42:47 +0100

    productDao updateQuantity query

--- src/main/java/karol/shop/repositories/IProductDao.java
+++ src/main/java/karol/shop/repositories/IProductDao.java
@@ -2,8 +2,12 @@ package karol.shop.repositories;
 
 import karol.shop.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface IProductDao extends JpaRepository<Product, Long> {
+        @Modifying
+        @Query("UPDATE Product p SET p.quantity = :quantity WHERE p.productId = :productId")
         void updateQuantity(Integer quantity, Long productId);
 
 }
