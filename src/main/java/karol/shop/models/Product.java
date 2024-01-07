1d218a75c5c0b7a807983cc2c65efaaa9a4e1b64
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2024-01-07 12:55:18 +0100

    changed back photo to photoUrl

--- src/main/java/karol/shop/models/Product.java
+++ src/main/java/karol/shop/models/Product.java
@@ -14,8 +14,8 @@ public class Product
     private long productId;
     private String modelId;
     private String title;
-    @Lob
-    private Byte[] photo;
+
+    private String photoUrl;
     private double price;
     private double deliveryPrice;
     private String description;
@@ -23,11 +23,11 @@ public class Product
     private String details;
 
     public Product() {}
-    public Product(long productId, String modelId, String title, byte[] photoUrl, double price, double deliveryPrice, String description, int quantity, String details) {
+    public Product(long productId, String modelId, String title, String photoUrl, double price, double deliveryPrice, String description, int quantity, String details) {
         this.productId = productId;
         this.modelId = modelId;
         this.title = title;
-        this.photo = photo;
+        this.photoUrl = photoUrl;
         this.price = price;
         this.deliveryPrice = deliveryPrice;
         this.description = description;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO PRODUCT (product_id, model_id, title, photo, price, delivery_price, description, quantity, details)
+INSERT INTO PRODUCT (product_id, model_id, title, photo_url, price, delivery_price, description, quantity, details)
 VALUES
     (1,'MODEL001', 'iPhone 13', 'https://www.mediaexpert.pl/media/cache/resolve/gallery/images/32/3225298/APPLE_iPhone_13__front_czarny.jpg', 799.99, 9.99, 'Nowoczesny smartfon z wieloma funkcjami.', 100, 'Specyfikacje: ...'),
     (2,'MODEL002', 'MacBook Air M1', 'https://a.allegroimg.com/s512/112cf6/59c293444b2bb370b08cc62f5ed6/Laptop-Apple-MacBook-Air-M1-3-2-8GB-128GB-Szary-2020-Kod-producenta-A2337', 1299.99, 19.99, 'Wydajny laptop idealny do pracy i rozrywki.', 50, 'Specyfikacje: ...');
